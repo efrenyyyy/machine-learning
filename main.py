312c7d4e5c26f7bd6814ea26acf21c8d60f74cae
efrenyyyy <efren_yang@163.com>
2017-01-12 15:47:19 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import kNN
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+
+group, labels = kNN.createDataSet()
+print kNN.classify0([0,1], group, labels, 3)
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+print datingDataMat
+print array(datingLabels)
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,0], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()

ee0a4becdbf0901c0ec156c19f2924f3e3152025
efrenyyyy <efren_yang@163.com>
2017-01-13 11:55:38 +0800

    数字识别

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import operator
+from os import listdir
 
 def createDataSet():
     group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -69,3 +70,38 @@ def classifyPersion():
     inArr = np.array([percetTats, ffMiles, iceCream])
     classifyResult = classify0((inArr-minValues)/ranges, normMat, datingLabels, 3)
     print "You will probably like this person: ", resultList[classifyResult]
+
+def img2vector(filename):
+    returnVect = np.zeros((1,1024))
+    f = open(filename)
+    for i in range(32):
+        listStr = f.readline()
+        for j in range(32):
+            returnVect[0, i*32+j] = listStr[j]
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' %fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with %d, the real answer is %d" %(classifierResult, classNumStr)
+        if (classifierResult != classNumStr):
+            errorCount += 1
+    print "the total number of errors is %d" %errorCount
+    print "the total error rate is %f" %(errorCount/float(mTest))
